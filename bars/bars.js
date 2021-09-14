71674a1ea8e61fd43f7f7ef5d2eba743ae87ffac
Hítalo Silva <hitalos@gmail.com>
2021-09-14 01:27:01 -0300

    Removing IIFE (This is already the esbuild output default format)

--- bars/bars.js
+++ bars/bars.js
@@ -6,150 +6,148 @@ import {
 	scaleLinear,
 } from 'd3'
 
-(() => {
-	const sortByConfirmed = document.getElementById('sortByConfirmed')
-	const sortByDeaths = document.getElementById('sortByDeaths')
+const sortByConfirmed = document.getElementById('sortByConfirmed')
+const sortByDeaths = document.getElementById('sortByDeaths')
 
-	const margins = { top: 50, left: 10, bottom: 30, right: 10 }
+const margins = { top: 50, left: 10, bottom: 30, right: 10 }
 
-	const cValues = (d) => parseInt(d.confirmed, 10)
-	const dValues = (d) => parseInt(d.deaths, 10)
-	const yValues = (d) => sortByConfirmed.checked ? cValues(d) : dValues(d)
-	const xValues = (d) => d.state
-	const updateValues = (d) => (new Date(...d.date.split('-'))).toLocaleDateString()
+const cValues = (d) => parseInt(d.confirmed, 10)
+const dValues = (d) => parseInt(d.deaths, 10)
+const yValues = (d) => sortByConfirmed.checked ? cValues(d) : dValues(d)
+const xValues = (d) => d.state
+const updateValues = (d) => (new Date(...d.date.split('-'))).toLocaleDateString()
 
-	class Bars {
-		constructor(parent) {
-			parent.innerText = ''
+class Bars {
+	constructor(parent) {
+		parent.innerText = ''
 
-			this.createSVG(parent)
+		this.createSVG(parent)
+		this.getDataAndMount(parent.dataset['url'])
+
+		sortByConfirmed.addEventListener('change', () => {
 			this.getDataAndMount(parent.dataset['url'])
-
-			sortByConfirmed.addEventListener('change', () => {
-				this.getDataAndMount(parent.dataset['url'])
-			})
-			sortByDeaths.addEventListener('change', () => {
-				this.getDataAndMount(parent.dataset['url'])
-			})
-		}
-
-		createSVG(parent) {
-			this.width = parent.dataset['width']
-			this.height = parent.dataset['height']
-			this.svg = select(parent)
-				.append('svg')
-				.attr('width', this.width)
-				.attr('height', this.height)
-				.attr('viewBox', `0 0 ${this.width} ${this.height}`)
-				.style('filter', 'drop-shadow(3px 3px 2px rgb(0 0 0 / .4))')
-
-			this.innerWidth = this.width - margins.left - margins.right
-			this.innerHeight = this.height - margins.top - margins.bottom
-
-			this.setTitle(parent.title)
-			this.setBorder()
-
-			this.gBars = this.svg.append('g')
-				.attr('transform', `translate(${margins.left}, ${margins.top})`)
-				.attr('class', 'bars')
-
-			this.gLabels = this.svg.append('g')
-				.attr('transform', `translate(${margins.left}, ${this.innerHeight + margins.top + 16})`)
-				.attr('class', 'labels')
-		}
-
-		getDataAndMount(url) {
-			csv(url).then((data) => {
-				this.data = data
-				this.data.sort((a, b) => yValues(a) - yValues(b))
-
-				this.scaleX = scaleBand()
-					.domain(this.data.map(xValues))
-					.range([0, this.innerWidth])
-					.padding(0.1)
-
-				this.scaleY = scaleLinear()
-					.domain([max(this.data.map(yValues)), 0])
-					.range([0, this.innerHeight])
-
-				this.scaleColor = scaleLinear()
-					.domain([0, this.data.length])
-					.range(['#597', '#3333aa'])
-
-				this.setBars()
-					.setLabels()
-			})
-		}
-
-		setTitle(title) {
-			this.svg.append('text')
-			.text(title)
-			.attr('text-anchor', 'middle')
-			.attr('x', this.width / 2)
-			.attr('y', margins.top)
-			.style('font-size', '2em')
-
-			return this
-		}
-
-		setBorder() {
-			this.svg.append('rect')
-				.attr('stroke', 'black')
-				.attr('fill', 'none')
-				.attr('width', this.width)
-				.attr('height', this.height)
-
-			return this
-		}
-
-		setBars() {
-			const bars = this.gBars.selectAll('rect').data(this.data, yValues)
-
-			bars.enter()
-				.append('rect')
-				.on('mouseover', (ev) => select(ev.target).style('stroke', 'black'))
-				.on('mouseout', (ev) => select(ev.target).style('stroke', 'none'))
-				.style('fill', (_, i) => this.scaleColor(i))
-				.attr('x', (_, i) => i * this.scaleX.step())
-				.attr('width', this.scaleX.bandwidth())
-				.attr('y', this.innerHeight)
-				.attr('height', 0)
-				.transition().duration(500)
-				.attr('y', (d) => this.scaleY(yValues(d)))
-				.attr('height', (d) => this.innerHeight - this.scaleY(yValues(d)))
-
-			bars.enter()
-				.append('title')
-				.text((d) => `Estado: ${xValues(d)}\nCasos confirmados: ${cValues(d).toLocaleString()}\nMortes: ${dValues(d).toLocaleString()}\nÚltima atualização: ${updateValues(d)}`)
-
-			bars.style('fill', sortByDeaths.checked ? 'black' : (_, i) => this.scaleColor(i))
-				.attr('y', this.innerHeight)
-				.attr('height', 0)
-				.transition().duration(500)
-				.attr('y', (d) => this.scaleY(yValues(d)))
-				.attr('height', (d) => this.innerHeight - this.scaleY(yValues(d)))
-
-			bars.append('title')
-				.text((d) => `Estado: ${xValues(d)}\nCasos confirmados: ${cValues(d).toLocaleString()}\nMortes: ${dValues(d).toLocaleString()}\nÚltima atualização: ${updateValues(d)}`)
-
-			return this
-		}
-
-		setLabels() {
-			const labels = this.gLabels.selectAll('text').data(this.data)
-
-			labels.enter()
-				.append('text')
-				.text((d) => xValues(d))
-				.style('font-size', '.7em')
-				.style('cursor', 'help')
-				.attr('text-anchor', 'middle')
-				.attr('x', (_, i) => (i * this.scaleX.step()) + this.scaleX.step() / 2)
-
-			return this
-		}
+		})
+		sortByDeaths.addEventListener('change', () => {
+			this.getDataAndMount(parent.dataset['url'])
+		})
 	}
 
-	document.querySelectorAll('.bars')
-		.forEach((el) => new Bars(el))
-})()
+	createSVG(parent) {
+		this.width = parent.dataset['width']
+		this.height = parent.dataset['height']
+		this.svg = select(parent)
+			.append('svg')
+			.attr('width', this.width)
+			.attr('height', this.height)
+			.attr('viewBox', `0 0 ${this.width} ${this.height}`)
+			.style('filter', 'drop-shadow(3px 3px 2px rgb(0 0 0 / .4))')
+
+		this.innerWidth = this.width - margins.left - margins.right
+		this.innerHeight = this.height - margins.top - margins.bottom
+
+		this.setTitle(parent.title)
+		this.setBorder()
+
+		this.gBars = this.svg.append('g')
+			.attr('transform', `translate(${margins.left}, ${margins.top})`)
+			.attr('class', 'bars')
+
+		this.gLabels = this.svg.append('g')
+			.attr('transform', `translate(${margins.left}, ${this.innerHeight + margins.top + 16})`)
+			.attr('class', 'labels')
+	}
+
+	getDataAndMount(url) {
+		csv(url).then((data) => {
+			this.data = data
+			this.data.sort((a, b) => yValues(a) - yValues(b))
+
+			this.scaleX = scaleBand()
+				.domain(this.data.map(xValues))
+				.range([0, this.innerWidth])
+				.padding(0.1)
+
+			this.scaleY = scaleLinear()
+				.domain([max(this.data.map(yValues)), 0])
+				.range([0, this.innerHeight])
+
+			this.scaleColor = scaleLinear()
+				.domain([0, this.data.length])
+				.range(['#597', '#3333aa'])
+
+			this.setBars()
+				.setLabels()
+		})
+	}
+
+	setTitle(title) {
+		this.svg.append('text')
+		.text(title)
+		.attr('text-anchor', 'middle')
+		.attr('x', this.width / 2)
+		.attr('y', margins.top)
+		.style('font-size', '2em')
+
+		return this
+	}
+
+	setBorder() {
+		this.svg.append('rect')
+			.attr('stroke', 'black')
+			.attr('fill', 'none')
+			.attr('width', this.width)
+			.attr('height', this.height)
+
+		return this
+	}
+
+	setBars() {
+		const bars = this.gBars.selectAll('rect').data(this.data, yValues)
+
+		bars.enter()
+			.append('rect')
+			.on('mouseover', (ev) => select(ev.target).style('stroke', 'black'))
+			.on('mouseout', (ev) => select(ev.target).style('stroke', 'none'))
+			.style('fill', (_, i) => this.scaleColor(i))
+			.attr('x', (_, i) => i * this.scaleX.step())
+			.attr('width', this.scaleX.bandwidth())
+			.attr('y', this.innerHeight)
+			.attr('height', 0)
+			.transition().duration(500)
+			.attr('y', (d) => this.scaleY(yValues(d)))
+			.attr('height', (d) => this.innerHeight - this.scaleY(yValues(d)))
+
+		bars.enter()
+			.append('title')
+			.text((d) => `Estado: ${xValues(d)}\nCasos confirmados: ${cValues(d).toLocaleString()}\nMortes: ${dValues(d).toLocaleString()}\nÚltima atualização: ${updateValues(d)}`)
+
+		bars.style('fill', sortByDeaths.checked ? 'black' : (_, i) => this.scaleColor(i))
+			.attr('y', this.innerHeight)
+			.attr('height', 0)
+			.transition().duration(500)
+			.attr('y', (d) => this.scaleY(yValues(d)))
+			.attr('height', (d) => this.innerHeight - this.scaleY(yValues(d)))
+
+		bars.append('title')
+			.text((d) => `Estado: ${xValues(d)}\nCasos confirmados: ${cValues(d).toLocaleString()}\nMortes: ${dValues(d).toLocaleString()}\nÚltima atualização: ${updateValues(d)}`)
+
+		return this
+	}
+
+	setLabels() {
+		const labels = this.gLabels.selectAll('text').data(this.data)
+
+		labels.enter()
+			.append('text')
+			.text((d) => xValues(d))
+			.style('font-size', '.7em')
+			.style('cursor', 'help')
+			.attr('text-anchor', 'middle')
+			.attr('x', (_, i) => (i * this.scaleX.step()) + this.scaleX.step() / 2)
+
+		return this
+	}
+}
+
+document.querySelectorAll('.bars')
+	.forEach((el) => new Bars(el))
--- gauge/gauge.js
+++ gauge/gauge.js
@@ -2,81 +2,80 @@ import {
 	select,
 } from 'd3'
 
-(() => {
-	class Gauge {
-		constructor(el) {
-			if (el.title === '') el.title = el.innerText
-			el.innerText = ''
-			this.value = el.dataset['value']
-			this.id = Math.random().toString(36).substr(2)
-			this.startColor = el.dataset['startColor'] ? el.dataset['startColor'] : '#05a'
-			this.endColor = el.dataset['endColor'] ? el.dataset['endColor'] : '#0a5'
 
-			this.svg = select(el)
-				.append('svg')
-				.attr('width', '220')
-				.attr('height', '220')
-				.attr('viewBox', '-120 -120 240 240')
-				.style('filter', 'drop-shadow(5px 5px 5px rgb(0 0 0 / .4))')
+class Gauge {
+	constructor(el) {
+		if (el.title === '') el.title = el.innerText
+		el.innerText = ''
+		this.value = el.dataset['value']
+		this.id = Math.random().toString(36).substr(2)
+		this.startColor = el.dataset['startColor'] ? el.dataset['startColor'] : '#05a'
+		this.endColor = el.dataset['endColor'] ? el.dataset['endColor'] : '#0a5'
 
-			this.defText().defCircle().defGradient()
+		this.svg = select(el)
+			.append('svg')
+			.attr('width', '220')
+			.attr('height', '220')
+			.attr('viewBox', '-120 -120 240 240')
+			.style('filter', 'drop-shadow(5px 5px 5px rgb(0 0 0 / .4))')
 
-			return this
-		}
+		this.defText().defCircle().defGradient()
 
-		defGradient() {
-			const gradient = this.svg
-				.append('defs')
-				.append('linearGradient')
-				.attr('id', `linearGradient_${this.id}`)
-				.attr('x1', '0%')
-				.attr('y1', '0%')
-				.attr('x2', '100%')
-				.attr('y2', '100%')
-			gradient.append('stop')
-				.attr('offset', '0%')
-				.attr('stop-color', this.startColor)
-			gradient.append('stop')
-				.attr('offset', '100%')
-				.attr('stop-color', this.endColor)
-
-			return this
-		}
-
-		defCircle() {
-			this.svg
-				.append('circle')
-				.attr('cy', 0)
-				.attr('cx', 0)
-				.attr('r', 100)
-				.attr('fill', 'none')
-				.attr('stroke-width', 40)
-				.attr('stroke', `url(#linearGradient_${this.id})`)
-				.attr('stroke-linecap', 'round')
-				.attr('stroke-dashoffset', `${50*Math.PI}`)
-				.attr('stroke-dasharray', `1 ${(100 * 2 * Math.PI)}`)
-				.transition().duration(500)
-				.attr('stroke-dasharray', `${this.value * 2 * Math.PI} ${((100 - this.value) * 2 * Math.PI)}`)
-
-			return this
-		}
-
-		defText() {
-			this.svg
-				.append('text')
-				.text(`${this.value}%`)
-				.attr('x', 0)
-				.attr('y', 0)
-				.attr('text-anchor', 'middle')
-				.attr('fill', 'darkgray')
-				.attr('dy', '.3em')
-				.style('font-size', '3em')
-				.style('user-select', 'none')
-
-			return this
-		}
+		return this
 	}
 
-	document.querySelectorAll('.gauge')
-		.forEach((el) => new Gauge(el))
-})()
+	defGradient() {
+		const gradient = this.svg
+			.append('defs')
+			.append('linearGradient')
+			.attr('id', `linearGradient_${this.id}`)
+			.attr('x1', '0%')
+			.attr('y1', '0%')
+			.attr('x2', '100%')
+			.attr('y2', '100%')
+		gradient.append('stop')
+			.attr('offset', '0%')
+			.attr('stop-color', this.startColor)
+		gradient.append('stop')
+			.attr('offset', '100%')
+			.attr('stop-color', this.endColor)
+
+		return this
+	}
+
+	defCircle() {
+		this.svg
+			.append('circle')
+			.attr('cy', 0)
+			.attr('cx', 0)
+			.attr('r', 100)
+			.attr('fill', 'none')
+			.attr('stroke-width', 40)
+			.attr('stroke', `url(#linearGradient_${this.id})`)
+			.attr('stroke-linecap', 'round')
+			.attr('stroke-dashoffset', `${50*Math.PI}`)
+			.attr('stroke-dasharray', `1 ${(100 * 2 * Math.PI)}`)
+			.transition().duration(500)
+			.attr('stroke-dasharray', `${this.value * 2 * Math.PI} ${((100 - this.value) * 2 * Math.PI)}`)
+
+		return this
+	}
+
+	defText() {
+		this.svg
+			.append('text')
+			.text(`${this.value}%`)
+			.attr('x', 0)
+			.attr('y', 0)
+			.attr('text-anchor', 'middle')
+			.attr('fill', 'darkgray')
+			.attr('dy', '.3em')
+			.style('font-size', '3em')
+			.style('user-select', 'none')
+
+		return this
+	}
+}
+
+document.querySelectorAll('.gauge')
+	.forEach((el) => new Gauge(el))
